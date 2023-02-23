f71d363bdef633a63e1339f940c80ec2a20b890f
VictorChao996 <96094victor@gmail.com>
2023-02-23 11:07:39 +0800

    finish express route structure

--- index.js
+++ index.js
@@ -36,34 +36,54 @@ app.get('/healthcheck', (req, res) => {
 
 //TODO: User Login API
 app.post('/users', (req, res) => {
+
+    //若header有誤直接返回status code 400 to client
+    if(res.headers['content-type'] !== 'application/json'){
+        return res.status(400).json({
+            "error": "Client Error Requested"
+        });
+    }
+
     const requestDate  = req.get('request-date');
     const {name, email, password} = req.body;
     const check = checkInputFormat(name, email, password);
     if(check){
         const emailExist = checkEmailExist(email);
         if(emailExist){
-            res.status(403).json({
+            return res.status(403).json({
                 "error": "Email already exists"
+            });
+        }else{
+            const id = registerUser(name, email, password);
+            return res.status(200).json({
+                "data":{
+                    "user": {id: id, email, password},
+                    "date": requestDate
+                }
             });
         }
     }
     else{
-        res.status(400).json({
+        return res.status(400).json({
             'error':'Input is not valid.',
         });
     }
     
 
-    res.json({
-        "data":{
-            "user": {id: 8787, email, password},
-            "date": requestDate
-        }
-    });
 });
 
-//TODO: User Query API
-app.get('/users', (req,res)=>{
+/**
+ * TODO: User Login API
+ * * app.get(/users): 根據query返回使用者JSON資料
+ */
+app.get('/users', (req,res,err)=>{
+    
+    //if the headers content type is wrong, send the 400 error to client.
+    if(req.headers['content-type'] !== 'application/json'){
+        return res.status(400).json({
+            'error': 'Client Error Requested'
+        })
+    }
     const userId = req.query.id;
     const requestDate  = req.get('request-date');
     const user = getUserDataFromDatabase(userId);
@@ -99,10 +119,29 @@ function checkInputFormat(name, email, password) {
     }
     return false;
 }
-//TODO: 檢查email是否已經註冊(已存在DB中)
+
+/**
+ * * 根據傳入的email檢查是否已經存在DB中
+ * TODO: 檢查email是否已經註冊(已存在DB中)
+ * @param {String} email 
+ * @returns {boolean} 
+ */
 function checkEmailExist(email){
     return false;
 }
+
+/**
+ * TODO: 註冊新User資料到DB中
+ * * registerUser(): 根據創建的結果返回userID
+ * @param {String} name 
+ * @param {String} email 
+ * @param {String} password 
+ * @return {String} id
+ */
+function registerUser(name, email, password){
+    const id = "1";
+    return id;
+} 
 
 //TODO: 檢查DB中的user資料並返回，若無則回傳空{}
 function getUserDataFromDatabase(userId){
