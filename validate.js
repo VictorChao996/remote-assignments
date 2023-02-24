be5900c780f427282a9394ae5a8c5c6a21a2b88e
VictorChao996 <96094victor@gmail.com>
2023-02-24 16:28:23 +0800

    Update: 1. Separate validation and query functions from the index.js 2. Make the query functions be asynchronous (query.js)

--- validate.js
+++ validate.js
@@ -1,15 +1,33 @@
-//TODO: 完成下面三個function
+/**
+ * @author: Victor
+ * * This file is a collection of the validation functions.
+ */
 
+/**
+ * - 檢查name是否符合格式
+ * @param {String} name 
+ * @returns {Boolean} true if 符合格式
+ */
 const validateName = (name) => {
     const regex = /^[A-Za-z0-9]+$/;
     return regex.test(name);
   };
 
+  /**
+   * - 檢查email是否符合格式
+   * @param {String} email 
+   * @returns {Boolean} true if 符合格式
+   */
   const validateEmail = (email) => {
     const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return regex.test(email);
   };
 
+  /**
+   * - 檢查password是否符合格式
+   * @param {String} password 
+   * @returns {Boolean} true if 符合格式
+   */
 const validatePassword = (password) => {
     //計算各個字元出現的次數
     const numUppercase = (password.match(/[A-Z]/g) || []).length;
@@ -38,4 +56,25 @@ const validatePassword = (password) => {
     return true;
   };
    
-module.exports = {validateName, validateEmail, validatePassword};
+
+  /**
+   * - 檢查DateString是否有符合格式 **[Sun, 06 Nov 1994 08:49:37 GMT]**
+   * @param {String} dateString 
+   * @return {Boolean} true if dateString format is valid
+   */
+const validateDateFormat = function(dateString) {
+  const dateFormat = /^\w{3}, \d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2} \w{3}$/;
+  return dateFormat.test(dateString);
+}
+
+
+const checkInputFormat = function(name, email, password){
+  //若有一string為空，則回傳false
+  if(validateName(name) && validateEmail(email) && validatePassword(password)){
+    return true;
+  }
+  return false;
+}
+
+
+module.exports = {validateName, validateEmail, validatePassword, validateDateFormat,checkInputFormat};
